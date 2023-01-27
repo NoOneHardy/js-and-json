7e5d2a6cb1d77541dd57e8d0014519c172e0d252
NoOneHardy <silas.hardegger@outlook.com>
2023-01-27 08:21:26 +0100

    example

---
+++ main.js
@@ -0,0 +1,6 @@
+const para1 = document.querySelector('#json')
+
+// Hier sollte irgendwie das JSON file eingefügt werden
+// Aber ich verstehe nicht wie ich das machen soll, ohne dass dieses File ein Module wird
+
+para1.innerHTML = data.fname
